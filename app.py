8aa8e0fa42dac4063d7df38b08fe4cc6bc9ea80d
Sidharth S <97397011+sidharthsajith@users.noreply.github.com>
2024-08-02 13:35:32 +0530

    Added New Feature
    Before, the code just generated the story on the prompt given initially, now a While True Loop is added to give a loop of chain of revamps and make it a story chain.

--- app.py
+++ app.py
@@ -6,9 +6,24 @@ client = Groq(
     api_key="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"
 )
 
+prompt = str(input("Starting: ")),
+prompt = (prompt, ", Now write a complete story using this as a starting. Minimum of 500 lines and a with a suitable title"),
+prompt = str(prompt)
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ],
+    model="llama3-8b-8192",
+)
+story = chat_completion.choices[0].message.content
+print(story)
+
 while True:
-    prompt = str(input("Starting: ")),
-    prompt = (prompt, ", Now write a complete story using this as a starting. Minimum of 500 lines and a with a suitable title"),
+    revamp = str(input("Do you want to add anything to the story? \n 1) New Characters \n 2) New Plot \n 3) A Twist \n \t Please Elaborate what all revamps would you like to make to the story: "))
+    prompt = (story,"   This is the story so far, Now I want you to make some revamps like: ",revamp, "And give me the newly revamped story.")
     prompt = str(prompt)
     chat_completion = client.chat.completions.create(
         messages=[
@@ -19,5 +34,5 @@ while True:
         ],
         model="llama3-8b-8192",
     )
-    a = chat_completion.choices[0].message.content
-    print(a)
+    story = chat_completion.choices[0].message.content
+    print(story)
