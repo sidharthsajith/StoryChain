46fe64ca7183cae9c400e5314c2e59478276d664
Sidharth S <97397011+sidharthsajith@users.noreply.github.com>
2024-08-02 13:43:17 +0530

    Added a quit function

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 from groq import Groq
 
 client = Groq(
@@ -22,7 +20,10 @@ story = chat_completion.choices[0].message.content
 print(story)
 
 while True:
-    revamp = str(input("Do you want to add anything to the story? \n 1) New Characters \n 2) New Plot \n 3) A Twist \n \t Please Elaborate what all revamps would you like to make to the story: "))
+    revamp = str(input("Do you want to add anything to the story? \n 1) New Characters \n 2) New Plot \n 3) A Twist \n 4) No its fine (quit) \n \t Please Elaborate what all revamps would you like to make to the story: "))
+    if revamp.lower == quit:
+        print("GoodBye")
+        break
     prompt = (story,"   This is the story so far, Now I want you to make some revamps like: ",revamp, "And give me the newly revamped story.")
     prompt = str(prompt)
     chat_completion = client.chat.completions.create(
